3efab51460f8f9c1e07697ed21b977aeda400e54
paksuhan <nastya.sushina@yandex.ru>
2019-03-02 00:59:24 +0300

    start files was added

---
+++ algoritms/a3/io.h
@@ -0,0 +1,11 @@
+#ifndef __IO_H__
+#define __IO_H__
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int read_all(char *fname, double **all, int *all_count, int *nx, int *ny, double *x, double *y);
+
+
+#endif
