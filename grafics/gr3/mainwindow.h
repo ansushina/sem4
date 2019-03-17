69bb6f0244cc1a29f0e438067fc8ee73a05e143a
paksuhan <nastya.sushina@yandex.ru>
2019-03-18 00:01:48 +0300

    new project

--- grafics/gr3/mainwindow.h
+++ grafics/gr3/mainwindow.h
@@ -23,14 +23,26 @@ protected:
 private slots:
     void on_pushButton_clicked();
 
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_5_clicked();
+
 private:
-    void cda(QPainter &paint);
-    void br_1(QPainter &paint);
-    void br_2(QPainter &paint);
-    void br_3(QPainter &paint);
+    void cda(QPainter &paint, double x1, double y1, double x2, double y2, QColor color);
+    void br_1(QPainter &paint, double x1, double y1, double x2, double y2, QColor color);
+    void br_2(QPainter &paint, double x1, double y1, double x2, double y2, QColor color);
+    void br_3(QPainter &paint, double x1, double y1, double x2, double y2, QColor color);
     Ui::MainWindow *ui;
     double x1,y1,x2,y2;
     int i;
+    double bg_x1, bg_y1, bg_x2, bg_y2;
+    int bg_i;
+    QColor color;
+    QColor bg_color;
+    bool flag;
+    bool f;
 };
 
 #endif // MAINWINDOW_H
---
+++ grafics/graf3/mainwindow.h
@@ -0,0 +1,78 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QPainter>
+#include <QtCore>
+#include <QtGui>
+#include <QGraphicsScene>
+#define PI 3.14159265
+
+#include <Qpainter>
+
+class Point
+{
+public:
+    Point(){}
+    Point(int x, int y): x_(x),y_(y) {}
+protected:
+    QRectF boundingRect() const
+    {
+        return QRectF(QPoint(x_,y_), QPoint(x_, y_));
+    }
+
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+    {
+        painter->drawPoint(x_, y_);
+        Q_UNUSED(option);
+        Q_UNUSED(widget);
+    }
+private:
+    int x_;
+    int y_;
+};
+
+
+
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+
+
+
+    void on_pushButton_clicked();
+
+    void on_draw_clicked();
+
+    void on_choose_color_clicked();
+
+    void on_clean_but_clicked();
+
+    void on_wiz_button_clicked();
+
+private:
+        QColor color;
+        double x1,y1,x2,y2;
+        int i;
+    Ui::MainWindow *ui;
+    QGraphicsScene *scene;
+    QVector<Point*> point;
+    void cda(QPen pen);
+    void br_1(QPen pen);
+    void br_2(QPen pen);
+    void br_3(QPen pen);
+    void method();
+};
+
+#endif // MAINWINDOW_H
