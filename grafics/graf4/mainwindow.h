c1a2d9c8374bb521f91e5da391874cda0ca51131
paksuhan <nastya.sushina@yandex.ru>
2019-03-31 17:31:29 +0300

    start files were added

---
+++ grafics/graf4/mainwindow.h
@@ -0,0 +1,26 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QGraphicsScene>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QGraphicsScene *scene;
+    double xc, yc;
+    double r;
+};
+
+#endif // MAINWINDOW_H
