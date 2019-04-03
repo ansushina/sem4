7509fbd10d65c2c3ffb50ddddc1d5fa56e422b18
ansushina <nastya.sushina@yandex.ru>
2019-04-04 00:17:36 +0300

    changes

--- grafics/graf4/mainwindow.cpp
+++ grafics/graf4/mainwindow.cpp
@@ -1,7 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QMessageBox>
-
+#include "math.h"
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -17,13 +17,159 @@ MainWindow::~MainWindow()
 }
 
 #include <math.h>
+/*
+ * MainWindow::traditional_okr(QPen pen)
+  «(»
+  {
+>     //x=0;
+>     //y=r;
+>     //x=r
+>     //y=0
+>     //y=sqrt(r*r-x*x)
+  «r*r-x*x»
+      //lg=abs((xi+1)^2 + yi^2 - r*r)
+  «(»
+      //ld = (xi+1)+(yi-1)-r
+  «(»
+      //lb = xi*xi + (yi-1) -r
+  «(»
+      //пять вариантов
+      //пять: Нет такого файла или каталога
+      // d - разность квадратов расстояний от центра окружности
+
+      //до диагонального пиксела и до идеальной окружности
+      //до: Нет такого файла или каталога
+      // di = (xi+1)^2 + (yi-1)^2 -r*r
+  «(»
+      // di < 0 -> внутри, выбираем горизонтальный или диагональный
+      0: Нет такого файла или каталога
+      // вычислим Лг и Лд - Д1 = Лг-Лд
+    //Д1 мельше 0 - диагональный
+    // иначе горизонтальный
+    // ди Ю 0 - снаружи
+   // посчитаем Д2 = ЛД-ЛВ
+   //если меньше нуля - диагональный
+   //иначе -вертикальный
+    // ди = 0 - лежит на окружности д1 и д2
+4$     int x = 0;
+ int y = r;
+
+   int d = 2*(1-r);
+     int d1, d2;
+
+    while (x < r)
+>     {
+>         //d = (x+1)*(x+1) + (y-1)*(y-1) - r*r;
+       if (d < 0)
+>         {
+>             //1 или 2 случай
+>             d1 = abs((x+1)*(x+1) + y*y - r*r) - abs((x+1)*(x+1) + (y-1)*(y-1)-r*r);
+          if (d1 <= 0)
+>             {
+>                 //горизонтальный
+>                 x++;
+>                 d = d+2*x +1;
+>             }
+>             else
+            {
+>                 //диагональный
+>                 x++;
+>                 y--;
+>                 d=d +2*x -2*y+2;
+>             }
+
+
+Try: sudo apt install <deb name>
+
+y--:
+
+          }
+  «}»
+          else (d > 0)
+  «else»
+          {
+>             d2 = 2 * (d-x) - 1;
+  «(»
+              if (d2 <= 0)
+>             {
+>                 //диагональный
+>                 x++;
+>                 y--;
+>                 d=d +2*x -2*y+2;
+>             }
+>             else
+  «else»
+              {
+>                 // вертикальный
+>                 y--;
+>                 d = 2*y+1
+>             }
+
+y--:
+d:
+
+          }
+  «}»
+          else
+  «else»
+          {
+>             // диагольный?
+>         }
+
+
+      }
+  «}»
+
+*/
 void MainWindow::traditional_okr(QPen pen)
+{
+
+    //double dx = r;
+  / // double dy = r;
+  //  double l = r;
+   //
+   // double sx = dx/l;
+   // double sy = dy/l;
+    double x = 0;
+    double y = r;
+
+    while (x < r && y > 0)
+    {
+        scene->addEllipse(round(x)+xc,round(y)+yc,0.1,0.1,pen,QBrush(Qt::SolidPattern));
+        x++;
+        y = sqrt(r*r-x*x);
+    }
+    while (y > 0)
+    {
+        scene->addEllipse(round(x)+xc,round(y)+yc,0.1,0.1,pen,QBrush(Qt::SolidPattern));
+        y--;
+        x = sqrt(r*r-y*y);
+    }
+}
+
+void MainWindow::parametr_okr(QPen pen)
+{
+    // x = r*cos(t);
+    // y = r*sin(t);
+    double x = 0;
+    double y = r;
+    for (int t = 0; t < 3.14/2; t += 1/r)
+    {
+        scene->addEllipse(round(x)+xc,round(y)+yc,0.1,0.1,pen,QBrush(Qt::SolidPattern));
+        x = r*cos(t);
+        y = r*sin(t);
+    }
+}
+
+
+void MainWindow::brezenhem(QPen pen)
 {
 
     int x = 0;
     int y = r;
-    int di = (x+1)*(x+1) + (y-1)*(y-1)-r*r;
-    while (x < xc+r)
+    int di = 2*(1-r);
+
+    while (x < r && y > 0)
     {
 
         scene->addEllipse(x+xc,y+yc,0.1,0.1,pen,QBrush(Qt::SolidPattern));
@@ -31,7 +177,7 @@ void MainWindow::traditional_okr(QPen pen)
         {
             //vnutri
             //диагональный или горизонтальный
-            int d1 = abs((x+1)*(x+1) + y*y + r*r)-abs((x+1)*(x+1) + (y-1)*(y-1)-r*r);
+            int d1 = 2*di + 2*y -1;
             if (d1 < 0)
             {
                 // горизонт
@@ -58,7 +204,7 @@ void MainWindow::traditional_okr(QPen pen)
         {
             //snaruzi
             //диагональный или вертикальный
-            int d2 = abs((x+1)*(x+1) + (y-1)*(y-1)-r*r)-abs(x*x + (y-1)*(y-1) - r*r);
+            int d2 = 2*di-2*x-1;
             if (d2 < 0)
             {
                 // диагональный
@@ -98,15 +244,15 @@ void MainWindow::method()
 
         }
     }
-    else if (ui->radioButton_2->isChecked())
+    else if (ui->radioButton_3->isChecked())
     {
         if (ui->radioButton_norm->isChecked())
         {
-             //br_1(QPen(color,1,Qt::SolidLine));
+             brezenhem(QPen(color,1,Qt::SolidLine));
         }
         else if (ui->radioButton_fon->isChecked())
         {
-            //br_1(QPen(Qt::white,1,Qt::SolidLine));
+            brezenhem(QPen(Qt::white,1,Qt::SolidLine));
 
         }
         else
@@ -118,7 +264,7 @@ void MainWindow::method()
 
         }
     }
-    else if (ui->radioButton_3->isChecked())
+    else if (ui->radioButton_2->isChecked())
     {
         if (ui->radioButton_norm->isChecked())
         {
