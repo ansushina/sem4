a4d9afadb4571636031dbd7ce6aa0b0e9896d40f
ansushina <nastya.sushina@yandex.ru>
2019-04-25 22:28:17 +0300

    trying to find mistakes

--- a5/main.cpp
+++ a5/main.cpp
@@ -10,29 +10,28 @@ using namespace std;
 #define EPS 0.0001
 #define N 40
 
-const double E[] = {12.13, 20.98, 31.00, 45.00};
+const double E[4] = {12.13, 20.98, 31.00, 45.00};
 
-const double QT[] = {2000,4000,6000,8000,10000,12000,14000,16000,18000,20000,22000,24000,26000};
-const double Q[][13] = {{1,1,1,1.0001, 1.0025, 1.0198, 1.0895, 1.2827, 1.6973, 2.4616,
-                         3.6552, 5.3749, 7.6838},
-                        {4,4,4.1598, 4.3006, 4.4392, 4.5662, 4.6817, 4.7923, 4.9099,5.0511,
-                         5.2354, 5.4841, 5.8181},
-                        {5.5,5.5,5.5116, 5.9790, 6.4749, 6.9590, 7.4145, 7.8370,
-                         8.2289, 8.5970, 8.9509, 9.3018, 9.6621},
-                        {11,11,11,11,11,11,11,11,11,11,11,11,11},
-                        {15,15,15,15,15,15,15,15,15,15,15,15,15}};
+const double QT[13] = {2000,4000,6000,8000,10000,12000,14000,16000,18000,20000,22000,24000,26000};
+const double Q[5][13] = {
+    {1,1,1,1.0001, 1.0025, 1.0198, 1.0895, 1.2827, 1.6973, 2.4616, 3.6552, 5.3749, 7.6838},
+    {4,4,4.1598, 4.3006, 4.4392, 4.5662, 4.6817, 4.7923, 4.9099,5.0511,5.2354, 5.4841, 5.8181},
+    {5.5,5.5,5.5116, 5.9790, 6.4749, 6.9590, 7.4145, 7.8370, 8.2289, 8.5970, 8.9509, 9.3018, 9.6621},
+    {11,11,11,11,11,11,11,11,11,11,11,11,11},
+    {15,15,15,15,15,15,15,15,15,15,15,15,15}};
 
 double get_q(int i, double t)
 {
-
+    if (i >= 5)
+        cout << "find q going wrong" << endl;
     //int make_new_mas(int n, double main_x, double **x, double **y, double *all_x,
       // double *all_y, int all_count);
     //int interpolation(int n, double x, double *new_x, double *new_y, double *answ)
     double answ;
     int n = 2;
     double *x, *y;
-    make_new_mas(n,t,&x,&y, QT, Q[i], 13);
-    interpolation(n, t, x,y, &answ);
+    make_new_mas(n, t, &x, &y, QT, Q[i], 13);
+    interpolation(n, t, x, y, &answ);
     free(x);
     free(y);
     return answ;
@@ -57,17 +56,21 @@ const double Z[5] = {0,1,2,3,4};
 
 double find_deltae(int i, double t, double g)
 {
+    if (i >= 4)
+        cout << "find deltae going wrong" << endl;
     double g2 = g/2;
-    return 8.61*pow(10, -5) * t * log((1+Z[i+1]*Z[i+1]*g2)*(1+g2)/(1+Z[i]*Z[i]*g2));
+    return 8.61*pow(10, -5) * t * log((1 + Z[i+1]*Z[i+1] * g2) * (1 + g2) / (1 + Z[i]*Z[i]*g2));
 }
 
 double find_k(int i, double t, double g)
 {
+    if (i >= 4)
+        cout << "find k going wrong" << endl;
     return 4.83*pow(10,-3) * get_q(i+1,t)/get_q(i,t) * pow(t, 3/2) * exp((E[i] - find_deltae(i,t,g))*11603/t);
 }
-void find_all_k(double K[5], double t, double g)
+void find_all_k(double K[4], double t, double g)
 {
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 4; i++)
     {
         K[i] = find_k(i,t,g);
     }
@@ -100,7 +103,6 @@ double find_gamma(double v, double x[5], double t)
         g = (g2-g1)/2;
         double f1 = gamma(g,v,x,t);
         double f2 = gamma(g2,v,x,t);
-        //printf("f(%lf) = %lf\n", p, f1);
         if (f1 * f2 < 0)
         {
             g1 = g;
@@ -119,26 +121,26 @@ double n_2(double p, double t)
 {
     double x[5] = {2,-1,10,-25,-35};
     double v = -1;
-    double deltav;
+    double deltav = 1;
 
     double m1[7] = {1,-1,1};
     double m2[7] = {1, 0,-1,1};
     double m3[7] = {1,0,0,-1,1};
     double m4[7] = {1,0,0,0,-1,1};
-    double m5[7],m6[7];
+    double m5[7] = {0}, m6[7] = {0};
     double *matrix[6];
     matrix[0] = m1;
     matrix[1] = m2;
     matrix[2] = m3;
     matrix[3] = m4;
-    matrix[3] = m5;
-    matrix[4] = m6;
+    matrix[4] = m5;
+    matrix[5] = m6;
 
     double alpha = 0;
     double gamma = 0;
-    double K[4];
+    double K[4] = {0};
     find_all_k(K,t,gamma);
-    while (fabs(deltav/v) < 0.0001)
+    while (fabs(deltav/v) > 0.0001)
     {
         matrix[4][0] = exp(v);
         matrix[5][0] = -exp(v);
@@ -154,7 +156,7 @@ double n_2(double p, double t)
         matrix[4][6] = -(exp(v) - Z[1]*exp(x[1]) - Z[2]*exp(x[2]) - Z[3]*exp(x[3]) - Z[4]*exp(x[4]));
         matrix[5][6] = -(7242*p/t + alpha - exp(v) - exp(x[0]) - exp(x[1]) - exp(x[2]) - exp(x[3])-exp(x[4]));
         double **b = NULL;
-        int rc =  do_gauss(matrix,6,7,&b);
+        int rc =  do_gauss(matrix, 6, 7, &b);
         if (rc) break;
         v = v + b[0][0];
         deltav = b[0][0];
@@ -172,6 +174,7 @@ double n_2(double p, double t)
     for (int i  =0; i < 5; i++)
     {
         n[i] = exp(x[i]);
+        cout <<"n"<<i<<" "<< n[i] << " "<<endl;
     }
 
     double nt = 0;
@@ -206,7 +209,7 @@ double find_f(double p, double t0, double tw, int m, double pn, double tn)
     {
         z = i * h;
         t = T(z, t0, tw, m);
-        nt[i] = n_2(p,t);
+        nt[i] = n_1(p,t);
     }
     for (int i = 0; i < N+1; i++)
     {
@@ -223,12 +226,12 @@ int process(double t0, double tw, int m, double pn, double tn)
     double p1 = P1;
     double p2 = P2;
     double p = (p2+p1)/2;
-
-    while ((p2-p1) > EPS)
+    double f1 = 1,f2;
+    while (fabs(f1) > EPS)
     {
         p = (p2+p1)/2;
-        double f1 = find_f(p,t0,tw,m,pn,tn);
-        double f2 = find_f(p2,t0,tw,m,pn,tn);
+        f1 = find_f(p,t0,tw,m,pn,tn);
+        f2 = find_f(p2,t0,tw,m,pn,tn);
         printf("f(%lf) = %lf\n", p, f1);
         if (f1 * f2 < 0)
         {
@@ -239,6 +242,7 @@ int process(double t0, double tw, int m, double pn, double tn)
             p2 = p;
         }
     }
+
     cout <<"Answer is " << p << endl;
     return 0;
 }
