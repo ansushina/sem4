a0eb1ab780152d1addc2fa5e3024b96be23a7585
U-VLAB\user-lab01 <user-lab01@LAB-01-31.vlab.bmstu.ru>
2019-04-26 15:14:09 +0300

    doesnt work but right k

--- a5/main.cpp
+++ a5/main.cpp
@@ -66,14 +66,8 @@ double find_k(int i, double t, double g)
 {
     if (i >= 4)
         cout << "find k going wrong" << endl;
-    return 4.83*pow(10,-3) * get_q(i+1,t)/get_q(i,t) * pow(t, 3/2) * exp((E[i] - find_deltae(i,t,g))*11603/t);
-}
-void find_all_k(double K[4], double t, double g)
-{
-    for (int i = 0; i < 4; i++)
-    {
-        K[i] = find_k(i,t,g);
-    }
+    return 4.83*pow(10,-3) * get_q(i+1,t)/get_q(i,t) * pow(t, 3/2) * exp(-(E[i] -
+                                                                           find_deltae(i,t,g))*11603/t);
 }
 
 double find_alpha(double t, double g)
@@ -119,15 +113,24 @@ double find_gamma(double v, double x[5], double t)
 
 double n_2(double p, double t)
 {
-    double x[5] = {2,-1,10,-25,-35};
     double v = -1;
-    double deltav = 1;
+    double x[5] = {2,-1,10,-25,-35};
 
-    double m1[7] = {1,-1,1};
-    double m2[7] = {1, 0,-1,1};
-    double m3[7] = {1,0,0,-1,1};
+    double n[5] = {0};
+    for (int i  = 0; i < 5; i++)
+    {
+        n[i] = exp(x[i]);
+        //cout <<"n"<<i<<" "<< n[i] << " "<<endl;
+    }
+
+    double deltav,deltax1,deltax2,deltax3,deltax4,deltax5;
+
+    double m1[7] = {1,-1,1,0,0,0};
+    double m2[7] = {1,0,-1,1,0,0};
+    double m3[7] = {1,0,0,-1,1,0};
     double m4[7] = {1,0,0,0,-1,1};
-    double m5[7] = {0}, m6[7] = {0};
+    double m5[7] = {0,0,0,0,0, 0};
+    double m6[7] = {0,0,0,0,0, 0};
     double *matrix[6];
     matrix[0] = m1;
     matrix[1] = m2;
@@ -138,43 +141,63 @@ double n_2(double p, double t)
 
     double alpha = 0;
     double gamma = 0;
-    double K[4] = {0};
-    find_all_k(K,t,gamma);
-    while (fabs(deltav/v) > 0.0001)
+    double **delta = NULL;
+    do
     {
         matrix[4][0] = exp(v);
         matrix[5][0] = -exp(v);
         for (int i = 0; i < 5; i++)
         {
-            matrix[4][i+1]= -Z[i]*exp(x[i]);
-            matrix[5][i+1] = -exp(x[i]);
+            matrix[4][i+1]= -Z[i]*n[i];
+            matrix[5][i+1] = -n[i];
         }
-        matrix[0][6] = -(v + x[1] + x[0] - log(K[0]));
-        matrix[1][6] = -(v + x[2] + x[1] - log(K[1]));
-        matrix[2][6] = -(v + x[3] + x[2] - log(K[2]));
-        matrix[3][6] = -(v + x[4] + x[3] - log(K[3]));
-        matrix[4][6] = -(exp(v) - Z[1]*exp(x[1]) - Z[2]*exp(x[2]) - Z[3]*exp(x[3]) - Z[4]*exp(x[4]));
-        matrix[5][6] = -(7242*p/t + alpha - exp(v) - exp(x[0]) - exp(x[1]) - exp(x[2]) - exp(x[3])-exp(x[4]));
-        double **b = NULL;
-        int rc =  do_gauss(matrix, 6, 7, &b);
+        matrix[0][6] = -(v + x[1] - x[0] - log(find_k(0,t,gamma)));
+        matrix[1][6] = -(v + x[2] - x[1] - log(find_k(1,t,gamma)));
+        matrix[2][6] = -(v + x[3] - x[2] - log(find_k(2,t,gamma)));
+        matrix[3][6] = -(v + x[4] - x[3] - log(find_k(3,t,gamma)));
+        cout <<"k1 "<< find_k(0,t,gamma);
+        cout <<" k2 " <<find_k(1,t,gamma);
+        cout <<" k3 " <<find_k(2,t,gamma);
+        cout <<" k4 " <<find_k(3,t,gamma) << endl;
+
+        matrix[4][6] = -(exp(v) - Z[1]*n[1] - Z[2]*n[2] - Z[3]*n[3] - Z[4]*n[4]);
+        matrix[5][6] = -(7242*p/t + alpha - exp(v) - n[0] - n[1] - n[2] - n[3] - n[4]);
+        int rc = do_gauss(matrix, 6, 7, &delta);
         if (rc) break;
-        v = v + b[0][0];
-        deltav = b[0][0];
+        v = v + delta[0][0];
+        deltav = delta[0][0];
+        deltax1 = delta[1][0];
+        deltax2 = delta[2][0];
+        deltax3 = delta[3][0];
+        deltax4 = delta[4][0];
+        deltax5 = delta[5][0];
+
         for (int i = 1; i < 5; i++)
         {
-            x[i-1] = x[i-1] + b[i][0];
+            x[i-1] = x[i-1] + delta[i][0];
         }
-        free_matrix(b,6);
+        free_matrix(delta,6);
+        cout <<"v "<<v;
+        for (int i  = 0; i < 5; i++)
+        {
+            n[i] = exp(x[i]);
+            cout <<"n"<<i<<" "<< n[i] << " "<<endl;
+        }
 
         gamma = find_gamma(v,x,t);
-        find_all_k(K,t,gamma);
         alpha = find_alpha(t,gamma);
     }
-    double n[5] = {0};
-    for (int i  =0; i < 5; i++)
+    while (0&&fabs(deltav/v) < 0.0001 &&
+           fabs(deltax1/x[0]) < 0.0001 &&
+           fabs(deltax2/x[1]) < 0.0001 &&
+           fabs(deltax3/x[2]) < 0.0001 &&
+           fabs(deltax4/x[3]) < 0.0001 &&
+           fabs(deltax5/x[4]) < 0.0001);
+
+    for (int i  = 0; i < 5; i++)
     {
         n[i] = exp(x[i]);
-        cout <<"n"<<i<<" "<< n[i] << " "<<endl;
+        //cout <<"n"<<i<<" "<< n[i] << " "<<endl;
     }
 
     double nt = 0;
@@ -209,7 +232,7 @@ double find_f(double p, double t0, double tw, int m, double pn, double tn)
     {
         z = i * h;
         t = T(z, t0, tw, m);
-        nt[i] = n_1(p,t);
+        nt[i] = n_2(p,t);
     }
     for (int i = 0; i < N+1; i++)
     {
@@ -227,7 +250,7 @@ int process(double t0, double tw, int m, double pn, double tn)
     double p2 = P2;
     double p = (p2+p1)/2;
     double f1 = 1,f2;
-    while (fabs(f1) > EPS)
+    while (fabs(f1) > EPS )
     {
         p = (p2+p1)/2;
         f1 = find_f(p,t0,tw,m,pn,tn);
