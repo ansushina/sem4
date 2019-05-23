c49b3cccaa79530cd39cb9fd9f55d24a8e45573f
ansushina <nastya.sushina@yandex.ru>
2019-05-24 01:53:46 +0300

    still bad works

--- algoritms/a5/main.cpp
+++ algoritms/a5/main.cpp
@@ -3,6 +3,7 @@
 #include <math.h>
 #include "gauss.h"
 #include "interpolation.h"
+#include "stdio.h"
 using namespace std;
 
 #define P1 3
@@ -19,14 +20,24 @@ const double Q[5][13] = {
     {5.5,5.5,5.5116, 5.9790, 6.4749, 6.9590, 7.4145, 7.8370, 8.2289, 8.5970, 8.9509, 9.3018, 9.6621},
     {11,11,11,11,11,11,11,11,11,11,11,11,11},
     {15,15,15,15,15,15,15,15,15,15,15,15,15}};
+void print_array(double *array, int n)
+{
+    for (int i = 0; i < n; i++)
+        printf("%15e ", array[i]);
+    printf("\n");
+}
 
+void print_mtr(double *mtr[7])
+{
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j < 7; j++)
+            printf("%15e ", mtr[i][j]);
+        printf("\n");
+    }
+}
 double get_q(int i, double t)
 {
-    if (i >= 5)
-        cout << "find q going wrong" << endl;
-    //int make_new_mas(int n, double main_x, double **x, double **y, double *all_x,
-      // double *all_y, int all_count);
-    //int interpolation(int n, double x, double *new_x, double *new_y, double *answ)
     double answ;
     int n = 2;
     double *x, *y;
@@ -56,33 +67,33 @@ double n_1(double p, double t)
 
 double find_deltae(int i, double t, double g)
 {
-    if (i >= 4)
-        cout << "find deltae going wrong" << endl;
     double g2 = g*0.5;
-    return 8.61*10e-5 * t * log((1 + Z[i+1]*Z[i+1] * g2) * (1 + g2) / (1 + Z[i]*Z[i]*g2));
+    double k1 = 8.61*1e-5 * t;
+    return  k1 * log(((1 + Z[i+1]*Z[i+1] * g2) * (1 + g2)) / (1 + Z[i]*Z[i]*g2));
 }
 
 double find_k(int i, double t, double g)
 {
-    if (i >= 4)
-        cout << "find k going wrong" << endl;
-    return 4.83*10e-3 * get_q(i+1,t)/get_q(i,t) * pow(t, 1.5) * exp(-(E[i] -
-                                                                           find_deltae(i,t,g))*11603.0/t);
+    double t32 = pow(t, 1.5);
+    double k1 = 4.83*1e-3;
+    double k2 = 11603.0/t;
+    return k1 * (get_q(i+1,t)/get_q(i,t)) * t32* exp(- E[i] + find_deltae(i,t,g)*k2);
 }
 
 double find_alpha(double t, double g)
 {
-    return 0.285 * 10e-11 * pow(g*t,3);
+    return 0.285 * 1e-11 * pow(g*t,3);
 }
 
 double gamma(double g, double v, double x[5], double t)
 {
     double s = exp(v)/(1+g*0.5);
+
     for(int i = 1; i < 5; i++)
     {
         s+=exp(x[i])*Z[i]*Z[i]/(1+Z[i]*Z[i] * g*0.5);
     }
-    s *= 5.87 * 10e10 /t/t/t;
+    s *= 5.87 * 1e10 /t/t/t;
     return g*g - s;
 }
 
@@ -90,11 +101,11 @@ double find_gamma(double v, double x[5], double t)
 {
     double g1 = 0;
     double g2 = 3;
-    double g = (g2-g1)/2;
+    double g = (g2+g1)/2;
 
     while(fabs(g2-g1) > EPS)
     {
-        g = (g2-g1)/2;
+        g = (g2+g1)/2;
         double f1 = gamma(g,v,x,t);
         double f2 = gamma(g2,v,x,t);
         if (f1 * f2 < 0)
@@ -105,22 +116,42 @@ double find_gamma(double v, double x[5], double t)
         {
             g2 = g;
         }
-
     }
     return g;
+}
 
+void fill_matrix(double *mt[7], double *x, double v)
+{
+    for(int i = 0; i < 4; i++)
+    {
+        mt[i][0] = 1;
+    }
+    int j = 1;
+    for (int i = 0; i < 4; i++)
+    {
+         mt[i][j] = -1;
+         mt[i][j + 1] = 1;
+         j++;
+    }
+    mt[4][0] = exp(v);
+    for (int i = 1; i < 5; i++)
+       mt[4][i + 1] = -Z[i] * exp(x[i]);
+    mt[5][0] = -exp(v);
+    for (int i = 0; i < 5; i++)
+       mt[5][i + 1] = -exp(x[i]);
 }
 
 double n_2(double p, double t)
 {
     double v = -1;
-    double x[5] = {2,-1,10,-25,-35};
+    double x[5] = {2,-1,-2,-25,-35};
+    double alpha = 0;
+    double gamma = 0;
 
     double n[5] = {0};
     for (int i  = 0; i < 5; i++)
     {
         n[i] = exp(x[i]);
-        //cout <<"n"<<i<<" "<< n[i] << " "<<endl;
     }
 
     double deltav,deltax1,deltax2,deltax3,deltax4,deltax5;
@@ -129,8 +160,8 @@ double n_2(double p, double t)
     double m2[7] = {1,0,-1,1,0,0};
     double m3[7] = {1,0,0,-1,1,0};
     double m4[7] = {1,0,0,0,-1,1};
-    double m5[7] = {0,0,0,0,0, 0};
-    double m6[7] = {0,0,0,0,0, 0};
+    double m5[7] = {0};
+    double m6[7] = {0};
     double *matrix[6];
     matrix[0] = m1;
     matrix[1] = m2;
@@ -139,29 +170,19 @@ double n_2(double p, double t)
     matrix[4] = m5;
     matrix[5] = m6;
 
-    double alpha = 0;
-    double gamma = 0;
+
     double **delta = NULL;
     do
     {
-        matrix[4][0] = exp(v);
-        matrix[5][0] = -exp(v);
-        for (int i = 0; i < 5; i++)
+        fill_matrix(matrix,x,v);
+        for (int i = 0; i < 4; i++)
         {
-            matrix[4][i+1]= -Z[i]*n[i];
-            matrix[5][i+1] = -n[i];
+            matrix[i][6] = -(v + x[i+1] - x[i] - log(find_k(i,t,gamma)));
         }
-        matrix[0][6] = -(v + x[1] - x[0] - log(find_k(0,t,gamma)));
-        matrix[1][6] = -(v + x[2] - x[1] - log(find_k(1,t,gamma)));
-        matrix[2][6] = -(v + x[3] - x[2] - log(find_k(2,t,gamma)));
-        matrix[3][6] = -(v + x[4] - x[3] - log(find_k(3,t,gamma)));
-        //cout <<"k1 "<< find_k(0,t,gamma);
-        //cout <<" k2 " <<find_k(1,t,gamma);
-        //cout <<" k3 " <<find_k(2,t,gamma);
-       // cout <<" k4 " <<find_k(3,t,gamma) << endl;
 
         matrix[4][6] = -(exp(v) - Z[1]*n[1] - Z[2]*n[2] - Z[3]*n[3] - Z[4]*n[4]);
         matrix[5][6] = -(7242*p/t + alpha - exp(v) - n[0] - n[1] - n[2] - n[3] - n[4]);
+
         int rc = do_gauss(matrix, 6, 7, &delta);
         if (rc) break;
         v = v + delta[0][0];
@@ -172,33 +193,44 @@ double n_2(double p, double t)
         deltax4 = delta[4][0];
         deltax5 = delta[5][0];
 
-        for (int i = 1; i < 5; i++)
+        for (int i = 1; i <= 5; i++)
         {
             x[i-1] = x[i-1] + delta[i][0];
         }
-        free_matrix(delta,6);
-        //cout <<"v "<<v;
+
         for (int i  = 0; i < 5; i++)
         {
             n[i] = exp(x[i]);
-            //cout <<"n"<<i<<" "<< n[i] << " "<<endl;
         }
 
         gamma = find_gamma(v,x,t);
         alpha = find_alpha(t,gamma);
+        free_matrix(delta,6);
+
+        printf("v-x:\n");
+        printf("%15e ", v);
+        print_array(x, 5);
+        printf("delta:\n");
+        printf("%15e %15e %15e %15e %15e %15e\n", deltav, deltax1, deltax2, deltax3, deltax4,deltax5);
+        printf("gamma = %lf\n", gamma);
+        printf("matrix:\n");
+        print_mtr(matrix);
+
     }
-    while (0 &&fabs(deltav/v) < 0.0001 &&
-           fabs(deltax1/x[0]) < 0.0001 &&
-           fabs(deltax2/x[1]) < 0.0001 &&
-           fabs(deltax3/x[2]) < 0.0001 &&
-           fabs(deltax4/x[3]) < 0.0001 &&
-           fabs(deltax5/x[4]) < 0.0001);
+    while (fabs(deltav/v) > 0.0001 &&
+           fabs(deltax1/x[0]) > 0.0001 &&
+           fabs(deltax2/x[1]) > 0.0001 &&
+           fabs(deltax3/x[2]) > 0.0001 &&
+           fabs(deltax4/x[3]) > 0.0001 &&
+           fabs(deltax5/x[4]) > 0.0001);
 
     for (int i  = 0; i < 5; i++)
     {
         n[i] = exp(x[i]);
-        //cout <<"n"<<i<<" "<< n[i] << " "<<endl;
     }
+    printf("ne n1-5:\n");
+    printf("%15e ", exp(v));
+    print_array(n, 5);
 
     double nt = 0;
     for (int i  = 0; i < 5; i++)
@@ -250,12 +282,12 @@ int process(double t0, double tw, int m, double pn, double tn)
     double p2 = P2;
     double p = (p2+p1)/2;
     double f1 = 1,f2;
-    while (fabs(f1) > EPS )
+    while (fabs(p2-p1) > EPS )
     {
         p = (p2+p1)/2;
         f1 = find_f(p,t0,tw,m,pn,tn);
         f2 = find_f(p2,t0,tw,m,pn,tn);
-        printf("f(%lf) = %lf\n", p, f1);
+        printf("f(%e) = %e\n", p, f1);
         if (f1 * f2 < 0)
         {
             p1 = p;
@@ -272,6 +304,8 @@ int process(double t0, double tw, int m, double pn, double tn)
 
 int main()
 {
+    setbuf(stdout, NULL);
+
     cout << "Hello World!" << endl;
     double t0,tw,pn,tn;
     int m;
